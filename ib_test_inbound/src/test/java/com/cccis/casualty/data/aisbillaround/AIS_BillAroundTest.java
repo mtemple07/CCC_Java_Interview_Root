59e8db7c988838784025f610b5313e2867cff292
Michael Temple <Michael_Temple@aisreview.com>
2020-01-24 15:16:35 -0600

    Adding file IB_Test_Pipe_TEST.txt for interview

--- ib_test_inbound/src/test/java/com/cccis/casualty/data/aisbillaround/AIS_BillAroundTest.java
+++ ib_test_inbound/src/test/java/com/cccis/casualty/data/aisbillaround/AIS_BillAroundTest.java
@@ -1,11 +1,5 @@
 /**
  * Copyright (C) 2019 CCCIS Casualty (admin@aisreview.com)
- *
- * The software code contained herein is the property of CCCIS
- * and its subsidiaries and affiliates (collectively, “CCCIS”).
- *
- * Use of this software code and any applications and information therein
- * constitutes acknowledgement of and consent to this notice
  */
 package com.cccis.casualty.data.aisbillaround;
 
